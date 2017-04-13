e800d8cba38a79c15189aed59e661eda9da63914
joengelm <joseph_engelman@brown.edu>
2017-04-13 02:05:03 -0400

    stuff works

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var stats;
 var camera, controls, scene, renderer, raycaster;
 const videos = {};
 
+const desc = {
+	welcome: 'Sweep is a collection of parallel realities. Every channel on Sweep is a different version of the world, created by you and other users.',
+	place_item: 'Place Content into a channel by copying it from the Web or by rendering a model yourself.',
+	virality: 'Objects with more views and appreciation will spread their appearance and behavior to nearby Content.',
+	avalanche: 'Content that begins to spread rapidly can quickly spiral out of control and burn out in a storm or avalanche of Content.',
+	behavior: 'Content can be assigned behavior. Choose from a selection of "causes" and connect them to various "effects" to make Content that interacts with its world.',
+	garden: 'Not all Content ends in an avalanche. This Lamp/Bear garden has found a quiet equilibrium of attention.',
+	memehell: 'Channels are customizable and easy to create. Anyone can create a new channel in under a minute.'
+}
+
 function useAccel() {
 	var nav = navigator.userAgent;
 	var mobile = false;
@@ -52,10 +62,13 @@ function init() {
 	
 	createSkybox();
 	//createContent();
-	//addGalleryImage('drive.gif', 1, 1);
+	addGalleryImage('images/1.png', 1, 1);
+	addGalleryImage('images/2.png', 2, 3);
+	addGalleryImage('images/3.png', 0, 1);
+	addGalleryImage('images/4.png', 1, 0);
 	setupVideos();
 	//createText();
-	//createObjects();
+	createObjects();
 		
 	window.addEventListener( 'resize', onWindowResize, false );
 
@@ -84,7 +97,6 @@ function onClick(event) {
 		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 		raycaster.setFromCamera( mouse, camera );
 		var intersects = raycaster.intersectObjects( scene.children ).map(function(p){return p.object});
-		console.log(intersects);
 		var obj = intersects.length ? intersects[0] : null;
 		setSelectedObject(obj);
 	}
@@ -123,30 +135,34 @@ function setSelectedObject(obj){
 	if (obj.name in videos) {
 		v = videos[obj.name];
 		vid = v.video;
-		console.log(obj.name);
 		vid.src = '';
 
 		const f = document.getElementById('fullscreen');
 		f.src = 'videos/' + obj.name + '.mp4';
+		f.loop = true;
 		f.style.zIndex = 1000;
+		f.muted = true;
 		f.play();
 
 		document.getElementById('overlay').style.zIndex = 999;
+		document.getElementById('descriptionText').innerHTML = desc[obj.name];
+		document.getElementById('description').style.bottom = 0;
+		document.getElementById('container').style.filter = 'blur(25px)';
 
 		inFullscreen = true;
 
 		f.addEventListener('click', e => {
-			console.log('fullscreen clicked');
 			f.src = '';
 			f.style.zIndex = -100;
 			document.getElementById('overlay').style.zIndex = -100;
+			document.getElementById('container').style.filter = 'none';
+			document.getElementById('description').style.bottom = '-200px';
 
 			inFullscreen = false;
 			
 			vid = videos[obj.name].video;
 			vid.src = 'videos/' + obj.name + '.mp4';
 			vid.play();
-			console.log(vid);
 		});
 	}
 	if (obj !== _selectedObject) {
@@ -159,7 +175,7 @@ function render() {
 }
 
 function createSkybox() {
-	const boxes = ['Park3Med/', 'MilkyWay/dark-s_', 'SwedishRoyalCastle/', 'skybox/'];
+	const boxes = ['Park3Med/', 'MilkyWay/dark-s_', 'SwedishRoyalCastle/'];
 	const r = 'cube/' + boxes[Math.floor(Math.random() * boxes.length)];
 	var ext = '.jpg';
 	var urls = [ r + "px" + ext, r + "nx" + ext,
@@ -207,7 +223,9 @@ function addGalleryImage(url, angle, vertAngle, link) {
 		 	plane.rotateY(angle * Math.PI / 180 + Math.PI/2);
 			plane.rotateX(vertAngle * Math.PI / 180);
 			plane.rotateZ((Math.random() - 0.5) * 2 * Math.PI * 2 * 0.1)
-		 	plane.translateZ(-5);
+		 	plane.translateZ(Math.random() * 20 - 10);
+		 	plane.translateX(Math.random() * 20 - 10);
+		 	rotatingObjects.push(plane);
 		 	scene.add( plane );
 			plane.selectable = true;
 			plane.link = link
@@ -227,27 +245,19 @@ function createObjects() {
 	loadObj('hand', function(object) {
 		object.rotateX(Math.PI/2);
 		object.scale.set(100,100,100);
-		object.position.y = -30;
+		object.position.y = -2;
 		rotatingObjects.push(object);
 		scene.add( object );
-	})
+	});
 	loadObj('justin', function(object) {
-		object.position.y = -3;
+		object.position.y = -10;
 		object.position.x = 5;
 		object.position.z = 10;
 		object.rotateZ(Math.PI/2);
 		object.scale.set(5,5,5);
 		rotatingObjects.push(object);
 		scene.add(object);
-	})
-	loadObj('mao', function(object) {
-		object.rotateX(-Math.PI*0.15);
-		object.rotateZ(-Math.PI*0.46);
-		object.rotateY(-Math.PI*0.05);
-		object.position.y = 50;
-		object.scale.set(50,50,50);
-		scene.add(object);
-	})
+	});
 }
 
 function loadObj(name, callback) {
@@ -314,25 +324,24 @@ function setupVideos() {
 	// VIDEO //
 	///////////
 
-	videoNames = ['intro', 'short'];
+	videoNames = ['welcome', 'place_item', 'virality', 'avalanche', 'behavior', 'memehell', 'garden'];
 
 	for (const name of videoNames) {
 		// create the video element
 		video = document.createElement('video');
 		video.id = name;
-		// video.type = ' video/ogg; codecs="theora, vorbis" ';
 		video.src = 'videos/' + name + '.mp4';
+		video.muted = true;
 		video.play();
 
 		video.addEventListener('ended', e => {
-			console.log('target', e.target);
 			e.target.currentTime = 0;
 			e.target.play();
 		});
 		
 		videoImage = document.createElement('canvas');
-		videoImage.width = 1088;
-		videoImage.height = 1088;
+		videoImage.width = 720;
+		videoImage.height = 720;
 
 		videoImageContext = videoImage.getContext('2d');
 		// background color if no video present
@@ -354,14 +363,55 @@ function setupVideos() {
 		const plane = new THREE.Mesh(geo, movieMaterial);
 		plane.name = name;
 
-		if (name === 'intro') {
+		if (name === 'welcome') {
 			plane.position.set(0,0,-30);
 			scene.add(plane);
 			camera.lookAt(plane.position);
-		} else {
-			const x = 25 + Math.floor(Math.random() * 10);
-			const y = 25 + Math.floor(Math.random() * 10);
-			const z = 25 + Math.floor(Math.random() * 10);
+		} else if (name === 'place_item') {
+			const x = 30;
+			const y = 25;
+			const z = -30;
+
+			plane.position.set(x,y,z);
+			scene.add(plane);
+			plane.lookAt(camera.position);
+		} else if (name === 'virality') {
+			const x = -30;
+			const y = 20;
+			const z = 30;
+
+			plane.position.set(x,y,z);
+			scene.add(plane);
+			plane.lookAt(camera.position);
+		} else if (name === 'avalanche') {
+			const x = 30;
+			const y = 0;
+			const z = 30;
+
+			plane.position.set(x,y,z);
+			scene.add(plane);
+			plane.lookAt(camera.position);
+		} else if (name === 'garden') {
+			const x = 20;
+			const y = 50;
+			const z = 30;
+
+			plane.position.set(x,y,z);
+			scene.add(plane);
+			plane.lookAt(camera.position);
+		} else if (name === 'behavior') {
+			const x = -30;
+			const y = 40;
+			const z = -30;
+
+			plane.position.set(x,y,z);
+			scene.add(plane);
+			plane.lookAt(camera.position);
+		} else if (name === 'memehell') {
+			const x = -40;
+			const y = -30;
+			const z = -35;
+
 			plane.position.set(x,y,z);
 			scene.add(plane);
 			plane.lookAt(camera.position);
